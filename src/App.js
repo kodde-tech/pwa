f7858beb3ed02f785da9b7b0a71b7ee2c2ab78a7
Gabriel Haruki <gabrielharukisatoh@gmail.com>
2021-09-27 10:27:23 -0300

    create pwa example

--- src/App.js
+++ src/App.js
@@ -1,8 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import { notification } from "antd";
+import logo from "./logo.svg";
+import "./App.css";
+
+import "antd/dist/antd.css";
 
 function App() {
+  const [deferredPrompt, setDeferredPrompt] = useState(null);
+  const [alreadyShownInstallNotification, setAlreadyShownInstallNotification] =
+    useState(false);
+
+  useEffect(() => {
+    window.addEventListener("beforeinstallprompt", (e) => {
+      // Prevent the mini-infobar from appearing on mobile
+      e.preventDefault();
+
+      setDeferredPrompt(e);
+    });
+
+    return () => {
+      window.removeEventListener("beforeinstallprompt");
+    };
+  }, []);
+
+  useEffect(() => {
+    deferredPrompt && showNotification();
+  }, [deferredPrompt]);
+
+  function showNotification() {
+    const notificationKey = "install-notification";
+
+    if (!alreadyShownInstallNotification) {
+      notification.open({
+        key: notificationKey,
+        message: "We have an app!",
+        description: "Would you like to install our app? Click here!",
+        duration: 0,
+        onClick: async () => {
+          deferredPrompt.prompt();
+          notification.close(notificationKey);
+        },
+      });
+    }
+
+    setAlreadyShownInstallNotification(true);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
